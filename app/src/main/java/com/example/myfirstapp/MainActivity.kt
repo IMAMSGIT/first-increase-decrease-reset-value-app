13566c78228265e323e92388fd21d24690289355
IMAMSGIT <imamhosain95@gmail.com>
2022-04-08 01:30:05 +0600

    increase decrease reset app

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -4,18 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AutoCompleteTextView
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     var count=0 // initialised a variable to increase latter
     private lateinit var countTextView: TextView // created a variable of TextView type
-
+    private lateinit var personImage:ImageView
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       countTextView=findViewById(R.id.countTV) // attached the textview to the button pressed
+        countTextView=findViewById(R.id.countTV) // attached the textview to the button pressed
+        personImage=findViewById(R.id.personIV) // set mipmap image to personImage object
+        personImage.setImageResource(R.drawable.count) // set count image to person image
     }
 
     fun clickToDecrease(view: View) {
